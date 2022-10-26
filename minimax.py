df18460e110c960e674d9d41d64e6b390bea9b7b
Jiseshen <116665077+Jiseshen@users.noreply.github.com>
2022-10-26 23:43:51 +0800

    Update minimax.py

--- minimax.py
+++ minimax.py
@@ -1,6 +1,3 @@
-from copy import deepcopy
-import time
-
 points = [0, 2, 8, 1e1, 1e2, 1e3, 1e4, 1e5]
 
 FIVE = 7
@@ -18,7 +15,7 @@ tolerance = 10
 
 max_depth = 5
 
-max_actions_num=10
+max_actions_num = 10
 
 
 class Board:
@@ -28,40 +25,39 @@ class Board:
         self.width = width
         self.height = height
         self.count = [[0 for chess_shape in range(8)] for role in range(2)]
-        self.involved = [[[0, 0, 0, 0] for x in range(width)] for y in range(height)]
+        self.involved = [[[0, 0, 0, 0] for y in range(height)] for x in range(width)]
 
-    def adjacent(self,x,y):
-        for i in range(-1,2):
-            for j in range(-1,2):
-                if i==0 and j==0:
+    def adjacent(self, x, y):
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                if i == 0 and j == 0:
                     pass
-                if not self.board[x][y]==0:
+                if not self.board[x][y] == 0:
                     return True
         return False
 
     def get_actions(self, role):
 
-        actions=[]
-        five=[]
-        live_four=[]
+        actions = []
+        five = []
+        live_four = []
         for x in range(self.width):
             for y in range(self.height):
-                if self.board[x][y] == 0 and self.adjacent(x,y):
-                    # x,y 可落子且附近有子
-                    m_score,o_score=self.point_score(x,y,role)
-                    action=(max(m_score,o_score),x,y)
-                    if m_score>=points[FIVE] or o_score>=points[FIVE]:
+                if self.board[x][y] == 0 and self.adjacent(x, y):
+                    m_score, o_score = self.point_score(x, y, role)
+                    action = (max(m_score, o_score), x, y)
+                    if m_score >= points[FIVE] or o_score >= points[FIVE]:
                         five.append(action)
-                    elif m_score>=points[FOUR] or o_score>=points[FOUR]:
+                    elif m_score >= points[FOUR] or o_score >= points[FOUR]:
                         live_four.append(action)
                     actions.append(action)
-        if len(five)>0:
+        if len(five) > 0:
             return five
-        if len(live_four)>0:
+        if len(live_four) > 0:
             return live_four
         actions.sort(reverse=True)
-        if len(actions)>max_actions_num:
-            actions=actions[0:max_actions_num]
+        if len(actions) > max_actions_num:
+            actions = actions[0:max_actions_num]
 
         return actions
 
@@ -72,13 +68,13 @@ class Board:
         self.board[x][y] = role
         for direction in directions:
             self.check_line(x, y, direction, role, self.count[role - 1])
-        m_score=self.get_point_score(role)
+        m_score = self.get_point_score(role)
         self.board[x][y] = 3 - role
         self.count = [[0 for chess_shape in range(8)] for role in range(2)]
         for direction in directions:
             self.check_line(x, y, direction, 3 - role, self.count[2 - role])
-        o_score=self.get_point_score(3-role)
-        return m_score,o_score
+        o_score = self.get_point_score(3 - role)
+        return m_score, o_score
 
     def get_point_score(self, role):
         score = 0
@@ -103,7 +99,6 @@ class Board:
         return score
 
     def make_line(self, x, y, direction, role):
-        # 在某个方向上制造直线
         line = [0 for _ in range(9)]
         tempx = x - 5 * direction[0]
         tempy = y - 5 * direction[1]
@@ -113,7 +108,6 @@ class Board:
             if 0 <= tempx <= self.width and 0 <= tempy <= self.height:
                 line[i] = self.board[tempx][tempy]
             else:
-                # 如果越界直接设置为对方棋子，这在棋型判断上不影响
                 line[i] = 3 - role
         return line
 
@@ -137,15 +131,12 @@ class Board:
         left = 4
         right = 4
         line = self.make_line(x, y, direction, role)
-
-        # 判断直线上的同类棋子的数量
         while left > 0 and line[left - 1] == role:
             left -= 1
         while right < 8 and line[right + 1] == role:
             right += 1
         left_edge = left
         right_edge = right
-        # 判断空子的数量
         while left_edge > 0 and line[left_edge - 1] != 3 - role:
             left_edge -= 1
         while right_edge < 8 and line[right_edge + 1] != 3 - role:
@@ -220,61 +211,85 @@ class Board:
                 count[TWO] += 1
         return 0
 
-    def get_score(self,role):
-        m_score=0
-        o_score=0
-        if self.count[role-1][FIVE]>0:
+    def get_score(self, role):
+        m_score = 0
+        o_score = 0
+        if self.count[role - 1][FIVE] > 0:
             return points[FIVE], 0
-        if self.count[2-role][FIVE]>0:
-            return 0,points[FIVE]
-        if self.count[role-1][SFOUR]>=2:
-            self.count[role-1][FOUR]+=1
-        if self.count[2-role][SFOUR]>=2:
-            self.count[2-role][FOUR]+=1
+        if self.count[2 - role][FIVE] > 0:
+            return 0, points[FIVE]
+        if self.count[role - 1][SFOUR] >= 2:
+            self.count[role - 1][FOUR] += 1
+        if self.count[2 - role][SFOUR] >= 2:
+            self.count[2 - role][FOUR] += 1
 
-        if self.count[role-1][FOUR]>0:
-            return 9050,0
-        if self.count[role-1][SFOUR]>0:
-            return 9040,0
+        if self.count[role - 1][FOUR] > 0:
+            return 9050, 0
+        if self.count[role - 1][SFOUR] > 0:
+            return 9040, 0
 
-        if self.count[2-role][FOUR]>0:
-            return 0,9030
-        if self.count[2-role][SFOUR]>0:
-            return 0,9020
+        if self.count[2 - role][FOUR] > 0:
+            return 0, 9030
+        if self.count[2 - role][SFOUR] > 0:
+            return 0, 9020
 
-        if self.count[role-1][THREE]>0 and self.count[2-role][SFOUR]==0:
-            return 9010,0
-        if self.count[2-role][THREE]>1 and self.count[role-1][THREE]==0 and self.count[role-1][STHREE]==0:
-            return 0,9000
+        if self.count[role - 1][THREE] > 0 and self.count[2 - role][SFOUR] == 0:
+            return 9010, 0
+        if self.count[2 - role][THREE] > 1 and self.count[role - 1][THREE] == 0 and self.count[role - 1][STHREE] == 0:
+            return 0, 9000
 
-        if self.count[2-role][SFOUR]>0:
-            o_score+=400
+        if self.count[2 - role][SFOUR] > 0:
+            o_score += 400
 
-        m_score+=
+        m_score += self.count[role - 1][THREE] * points[THREE]
+        m_score += self.count[role - 1][STHREE] * points[STHREE]
+        m_score += self.count[role - 1][TWO] * points[TWO]
+        m_score += self.count[role - 1][STWO] * points[STWO]
+        o_score += self.count[2 - role][THREE] * points[THREE]
+        o_score += self.count[2 - role][STHREE] * points[STHREE]
+        o_score += self.count[2 - role][TWO] * points[TWO]
+        o_score += self.count[2 - role][STWO] * points[STWO]
 
-    def utility(self,role):
+        return m_score, o_score
+
+    def utility(self, role):
+        self.count = [[0 for pattern in range(8)] for role in range(2)]
+        self.involved = [[[0, 0, 0, 0] for y in range(self.height)] for x in range(self.width)]
+        directions = [(1, 0), (0, 1), (1, 1), (1, -1)]
+        for y in range(self.height):
+            for x in range(self.width):
+                if self.board[x][y] == role:
+                    for i in range(4):
+                        if self.involved[x][y][i] == 0:
+                            self.check_line(x, y, directions[i], role, self.count[role - 1])
+                if self.board[x][y] == 3 - role:
+                    for i in range(4):
+                        if self.involved[x][y][i] == 0:
+                            self.check_line(x, y, directions[i], 3 - role, self.count[2 - role])
+
+        m_score, o_score = self.get_score(role)
+        return w * m_score - o_score
 
 
-def max_value(board, role, alpha, beta, depth, t):
-
-    if depth>=max_depth or (time.time() - t > 10):
-        return board.utility(),None
+def max_value(board, role, alpha, beta, depth):
+    if depth >= max_depth:
+        return board.utility(role), None
     v = float("-inf")
 
     action_list = board.get_actions(role)  # 获取自己可下的地方
     if len(action_list) != 0:  # 有地方可下
         action = None
         for a in action_list:
-            new_board = deepcopy(board)
-            new_board.update(a[0],a[1],role)
-            move_v, _ = min_value(new_board, role, alpha, beta, depth + 1)
+            board.board[a[1]][a[2]]=role
+            move_v, _ = min_value(board, role, alpha, beta, depth + 1)
+            board.board[a[1]][a[2]]=0
             if move_v > v:
                 v = move_v
                 action = a
             if v >= beta: return v, action
             alpha = max(alpha, v)
         return v, action
-            # ---------------------------------
+        # ---------------------------------
 
     else:  # 没有地方下
         v = board.utility()
@@ -282,37 +297,37 @@ def max_value(board, role, alpha, beta, depth, t):
     return v, action
 
 
-def min_value(board, role, alpha, beta, depth, t):
-
-    if depth>=max_depth or (time.time() - t > 10):
-        return board.utility(),None
+def min_value(board, role, alpha, beta, depth):
+    if depth >= max_depth:
+        return board.utility(), None
     v = float("inf")
 
-    action_list = board.get_actions(3-role)  # 获取对手可下的地方
+    action_list = board.get_actions(3 - role)  # 获取对手可下的地方
     if len(action_list) != 0:  # 有地方可下
         action = None
         for a in action_list:
-            new_board = deepcopy(board)
-            new_board.update(a[0],a[1],3-role)
-            move_v, _ = min_value(new_board, role, alpha, beta, depth + 1)
+            board.board[a[0]][a[1]]=3-role
+            move_v, _ = max_value(board, role, alpha, beta, depth + 1)
+            board.board[a[0]][a[1]]=0
             if move_v < v:
                 v = move_v
                 action = a
             if v <= alpha: return v, action
             beta = min(beta, v)
         return v, action
-            # ---------------------------------
+        # ---------------------------------
 
     else:  # 没有地方下
         v = board.utility()
         action = None
     return v, action
 
-def search(board, t):
-    P=Board(board)
-    alpha=float("-inf")
-    beta=float("inf")
-    depth=0
-    role=1
-    _,action=max_value(board,role,alpha,beta,depth, t)
+
+def search(board,width,height):
+    P = Board(board,width,height)
+    alpha = float("-inf")
+    beta = float("inf")
+    depth = 0
+    role = 1
+    _, action = max_value(P, role, alpha, beta, depth)
     return action
